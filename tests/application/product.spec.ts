82f67d8fb9e42041b07cc76af0fbeb2c3a50797e
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-05-15 10:19:52 -0300

    test: ensure to not enable a product if the price is 0 or lower

--- tests/application/product.spec.ts
+++ tests/application/product.spec.ts
@@ -1,9 +1,15 @@
 import Product, { ProductStatus } from '../../src/application/product'
 
 describe('Product Unit Tests', () => {
-  it('Should enable a Product', () => {
+  it('Should enable a Product if the price is major of 0', () => {
     const sut = new Product('1', 'Product 1', 12.7, ProductStatus.DISABLED)
     sut.enable()
     expect(sut.getStatus()).toBe(ProductStatus.ENABLED)
+  })
+
+  it('Should not enable the Product if the price is 0 or lower', () => {
+    const sut = new Product('1', 'Product 1', 0, ProductStatus.DISABLED)
+    sut.enable()
+    expect(sut.getStatus()).toBe(ProductStatus.DISABLED)
   })
 })
