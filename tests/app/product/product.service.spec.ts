5f6a9e5eccbc9a566aeb481b20bf071018d0fcf6
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-05-16 08:54:06 -0300

    test: ensure Product Service call getById method with correct id

--- tests/app/product/product.service.spec.ts
+++ tests/app/product/product.service.spec.ts
@@ -1,23 +1,32 @@
-import { IProductService } from '../../../src/app/product/interfaces'
-import { ProductStatus, ProductType } from '../../../src/app/product/types'
+import {
+  ProductService,
+  IProductService,
+  ProductStatus,
+  ProductType
+} from '../../../src/app/product'
 
 const mockedProducts: ProductType[] = [
-  { id: '1', name: 'Product 1', price: 12.7, status: ProductStatus.ENABLED }
+  { id: '1', name: 'Product 1', price: 12.7, status: ProductStatus.ENABLED },
+  { id: '2', name: 'Product 2', price: 0, status: ProductStatus.DISABLED }
 ]
 
-class ProductServiceStub implements IProductService {
-  get (): ProductType[] {
-    return mockedProducts
-  }
-}
-
-const makeSut = (): IProductService => new ProductServiceStub()
+const makeSut = (): IProductService => new ProductService()
 
 describe('Product Service Unit Tests', () => {
   it('Should call Get method with no params to get all', () => {
     const sut = makeSut()
+    jest.spyOn(sut, 'get').mockImplementationOnce(() => mockedProducts)
     const allProducts = sut.get()
     expect(allProducts?.length).toBeDefined()
     expect(allProducts).toEqual(mockedProducts)
+  })
+
+  it('Should call Get by Id method with valid id', () => {
+    const sut = makeSut()
+    const productId = '1'
+    jest.spyOn(sut, 'getById').mockImplementationOnce(() => mockedProducts[productId])
+    const foundProduct = sut.getById(productId)
+    expect(foundProduct).toBeDefined()
+    expect(foundProduct).toEqual(mockedProducts[productId])
   })
 })
